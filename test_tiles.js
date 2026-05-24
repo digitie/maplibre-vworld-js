3939df5ababc0065b8f28aff473a19e427a14c04
digitie <digitie@gmail.com>
2026-05-24 20:22:45 +0900

    fix: lower maximum zoom level to 17 to avoid VWorld's 200 OK 'no data' white tiles

---
+++ test_tiles.js
@@ -0,0 +1,31 @@
+const https = require('https');
+const fs = require('fs');
+const API_KEY = 'C737D607-1011-3AD8-98A0-EE550472F20A';
+
+function lon2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+
+const lon = 127.0246;
+const lat = 37.5326;
+
+async function checkZoom(z) {
+    const x = lon2tile(lon, z);
+    const y = lat2tile(lat, z);
+    const url = `https://api.vworld.kr/req/wmts/1.0.0/${API_KEY}/Base/${z}/${y}/${x}.png`;
+    
+    return new Promise((resolve) => {
+        https.get(url, (res) => {
+            const file = fs.createWriteStream(`z${z}.png`);
+            res.pipe(file);
+            file.on('finish', () => resolve());
+        });
+    });
+}
+
+async function run() {
+    await checkZoom(17);
+    await checkZoom(18);
+    await checkZoom(19);
+}
+
+run();
